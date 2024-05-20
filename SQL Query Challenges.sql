5a77ca0d2446a5197e645093fa496ee91b8bbd12
Abhishek Kumar <96631543+abhi191102@users.noreply.github.com>
2024-05-20 23:23:41 +0530

    SQL Query Challenges.sql

--- SQL Query Challenges.sql
+++ SQL Query Challenges.sql
@@ -9,12 +9,7 @@ LIMIT 5;
 
 /*What day of the week do most users register on?
 We need to figure out when to schedule an ad campgain*/
-SELECT date_format(created_at,'%W') AS 'day of the week', COUNT(*) AS 'total registration'
-FROM users
-GROUP BY 1
-ORDER BY 2 DESC;
 
-/*version 2*/
 SELECT 
     DAYNAME(created_at) AS day,
     COUNT(*) AS total
