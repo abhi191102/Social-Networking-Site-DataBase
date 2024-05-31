895f39ea6ced94ce8cea1175e50f4a1aab439b8e
Abhishek Kumar <96631543+abhi191102@users.noreply.github.com>
2024-05-31 16:54:10 +0530

    Update SQL Query Challenges.sql

--- SQL Query Challenges.sql
+++ SQL Query Challenges.sql
@@ -115,31 +115,9 @@ SELECT COUNT(*) FROM
 	HAVING comment_text IS NULL) AS total_number_of_users_without_comments;
 
 /*Mega Challenges
-Are we overrun with bots and celebrity accounts?
-Find the percentage of our users who have either never commented on a photo or have commented on every photo*/
+Are we overrun with bots and celebrity accounts?*/
 
-SELECT tableA.total_A AS 'Number Of Users who never commented',
-		(tableA.total_A/(SELECT COUNT(*) FROM users))*100 AS '%',
-		tableB.total_B AS 'Number of Users who likes every photos',
-		(tableB.total_B/(SELECT COUNT(*) FROM users))*100 AS '%'
-FROM
-	(
-		SELECT COUNT(*) AS total_A FROM
-			(SELECT username,comment_text
-				FROM users
-				LEFT JOIN comments ON users.id = comments.user_id
-				GROUP BY users.id
-				HAVING comment_text IS NULL) AS total_number_of_users_without_comments
-	) AS tableA
-	JOIN
-	(
-		SELECT COUNT(*) AS total_B FROM
-			(SELECT users.id,username, COUNT(users.id) As total_likes_by_user
-				FROM users
-				JOIN likes ON users.id = likes.user_id
-				GROUP BY users.id
-				HAVING total_likes_by_user = (SELECT COUNT(*) FROM photos)) AS total_number_users_likes_every_photos
-	)AS tableB;
+
 
 
 /*Find users who have ever commented on a photo*/
